72d1e4870577072120724d90fe54bc5f1b9d2f8a
raphaeldealmeida <jaguarnet7@gmail.com>
2014-11-08 15:46:57 -0200

    Perguntando o usuário

--- web/index.js
+++ web/index.js
@@ -11,10 +11,32 @@ app.get('/player', function(req, res){
     res.sendfile(__dirname + '/player.html');
 });
 
+var usernames = {};
+
 io.on('connection', function(socket){
   socket.on('board', function(msg){
     io.emit('board', msg);
   });
+
+    // when the client emits 'adduser', this listens and executes
+    socket.on('adduser', function(username){
+        // we store the username in the socket session for this client
+        socket.username = username;
+        // add the client's username to the global list
+        usernames[username] = username;
+        //// echo to client they've connected
+        //socket.emit('updatechat', 'SERVER', 'you have connected');
+        //// echo globally (all clients) that a person has connected
+        //socket.broadcast.emit('updatechat', 'SERVER', username + ' has connected');
+        //// update the list of users in chat, client-side
+        //io.sockets.emit('updateusers', usernames);
+        times = ['red', 'blue'];
+        time = times[Math.floor(Math.random()*times.length)];
+        io.sockets.emit('create', '{"id" : "'+username+'", "time": "'+time+'"}');
+    });
+
+
+
 });
 
 http.listen(3001, function(){
