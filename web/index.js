101578c20f73e2b7dad51767e678baed89abc0e9
raphaeldealmeida <jaguarnet7@gmail.com>
2014-11-08 13:12:19 -0200

    Server web

---
+++ web/index.js
@@ -0,0 +1,21 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.get('/', function(req, res){
+  res.sendfile(__dirname + '/board.html');
+});
+
+
+
+
+
+io.on('connection', function(socket){
+  socket.on('board', function(msg){
+    io.emit('board', msg);
+  });
+});
+
+http.listen(3001, function(){
+  console.log('listening on *:3001');
+});
